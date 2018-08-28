cca44aca3a8a2ee11478c3030a2bcc24e067c1f8
k.tashiro <tashiro@lepra.jp>
2018-08-28 18:26:33 +0900

    image fetcher

--- skimageSample.py
+++ skimageSample.py
@@ -16,7 +16,7 @@ print(threshold_value)
 project_root = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(project_root, 'sampleImage/olympus_cat.jpg')
 cat = io.imread(filename)
-print(type(cat), cat.shape)  # 225, 300, 3
+print(type(cat), cat.shape, len(cat))  # 225, 300, 3
 
 # xy(2次元) + rgb(+1次元) の3次元データを、xyへマッピング
 red_2d = np.apply_along_axis(lambda x: x[0], 2, cat)
@@ -24,4 +24,5 @@ red_2d = np.apply_along_axis(lambda x: x[0], 2, cat)
 # @SEE https://note.nkmk.me/python-seaborn-heatmap/
 plt.figure(figsize=(7, 4))
 sns.heatmap(red_2d)
+plt.imshow(cat)
 plt.show()
