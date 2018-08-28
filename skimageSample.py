bc50265b5870b30677b804028b8151e4967b57bc
k.tashiro <tashiro@lepra.jp>
2018-08-28 16:45:39 +0900

    skimage sample

---
+++ skimageSample.py
@@ -0,0 +1,27 @@
+import skimage
+import os
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import data, filters, io
+
+camera = data.camera()
+
+print(type(camera), camera.shape)
+
+coins = data.coins()
+threshold_value = filters.threshold_otsu(coins)
+print(threshold_value)
+
+project_root = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(project_root, 'sampleImage/olympus_cat.jpg')
+cat = io.imread(filename)
+print(type(cat), cat.shape)  # 225, 300, 3
+
+# xy(2次元) + rgb(+1次元) の3次元データを、xyへマッピング
+red_2d = np.apply_along_axis(lambda x: x[0], 2, cat)
+
+# @SEE https://note.nkmk.me/python-seaborn-heatmap/
+plt.figure(figsize=(7, 4))
+sns.heatmap(red_2d)
+plt.show()
